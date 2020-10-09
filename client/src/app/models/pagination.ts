4e31df57f679588bef6701dcecb771d0adc0f996
Matt Bridger <matt@wizard-tech.co.uk>
2020-10-09 11:17:43 +0100

    End of section 8

--- client/src/app/models/pagination.ts
+++
@@ -1,9 +0,0 @@
-import { IProduct } from './product';
-
-// remember to use export!
-export interface IPagination {
-  pageIndex: number;
-  pageSize: number;
-  count: number;
-  data: IProduct[];
-}
